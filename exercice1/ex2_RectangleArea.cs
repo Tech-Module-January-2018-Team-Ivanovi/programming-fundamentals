87beb573f921d79e7d7d503528b8e832738cc170
MartaIvanova <37123513+MartaIvanova@users.noreply.github.com>
2018-03-19 16:01:36 +0200

    Ready again

---
+++ exercice1/ex2_RectangleArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex02_RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = width * height;
+
+            Console.WriteLine("{0:F2}", area);
+        }
+    }
+}
